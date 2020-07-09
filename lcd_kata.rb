e3361c801c80619f1edc4f814101e594542978eb
jorge27 <ja.garciavega@outlook.com>
2020-07-09 18:00:54 -0500

    Session 2
    Jorge G. was here!!

--- lcd_kata.rb
+++ lcd_kata.rb
@@ -1,7 +1,22 @@
 
 class ConvertNumber
-  def initialize
-    
+	attr_reader :number
+  def initialize(number)
+    @number = number
+  end
+
+  def number?
+  	(number.to_i == 0) ? false : true
+  end
+
+  def 
+
+  def display_lcd
+  	case @number
+  	when 1
+  	
+  	end
+
   end
   
 end
--- spec/lcd_kata_spec.rb
+++ spec/lcd_kata_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe 'ConvertNumber' do
   let(:convert_number) { ConvertNumber.new }
   describe '#number?' do
     it 'Will let me know if the given character is a number or not' do
-      convert_number.number? = a
-      expect(convert_number.number?).to be(false)
+      convert_number.number? = 'a'
+      expect(convert_number.number?).to eq(false)
     end
   end
 end
