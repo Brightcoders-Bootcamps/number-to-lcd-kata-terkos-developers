f0a4ce1730a01c66f086abfaac661e7b207bc97c
Andres Pacheco <andrespach4@gmail.com>
2020-07-09 18:47:42 -0500

    solving kata

--- lcd_kata.rb
+++ lcd_kata.rb
@@ -1,6 +1,7 @@
 
 class ConvertNumber
 	attr_reader :number
+  
   def initialize(number)
     @number = number
   end
@@ -9,14 +10,124 @@ class ConvertNumber
   	(number.to_i == 0) ? false : true
   end
 
-  def 
-
   def display_lcd
-  	case @number
-  	when 1
-  	
-  	end
-
+    number_array = number.to_s.split("") # ["1", "2", "3"]
+    
+    number_array.each do |number_string|
+      puts hash_value[number_string]
+    end
   end
-  
+
+  def hash_value
+    {
+      "0" => zero,  
+      "1" => uno,
+      "2" => dos,
+      "3" => tres,
+      "4" => cuatro,
+      "5" => cinco,
+      "6" => seis, 
+      "7" => siete,
+      "8" => ocho,
+      "9" => nueve
+    }
+  end 
+    
+  def zero
+    " 
+      ____
+     |    |
+     |    |
+      ____
+        
+    "
+  end
+
+  def uno
+    " 
+      __
+        |
+        |
+      _____"
+  end
+
+  def dos
+    " 
+      __
+        |
+      __
+     |
+      __"
+  end
+
+  def tres
+    "
+     ___
+        |
+     ---|
+        |
+     ____
+    "
+  end
+
+  def cuatro
+    "
+     |   |
+     |___|
+         |
+         |
+    "
+  end
+
+  def cinco
+    "
+     ___
+    |
+     ---|
+        |
+     ____
+    "
+  end
+
+  def seis
+    "
+     ___
+    |   
+    |---|
+    |  |
+    ___
+    "
+  end
+
+  def siete
+    "
+     ___
+        |
+     ---|
+        |
+    "
+  end
+
+  def ocho
+    "
+     ___
+    |  |
+    ---|
+    |  |
+    ____
+    "
+  end
+
+  def nueve
+    "
+     ___
+    |    |
+     ---|
+        |
+        |
+    "
+  end
 end
+
+a = ConvertNumber.new(1234)
+a.display_lcd
