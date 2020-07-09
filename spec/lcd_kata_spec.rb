7fc4d238ed613544097e26222c7913040f33c0e5
Ana Arreola <laurarreola.ana@gmail.com>
2020-07-09 18:30:58 -0500

    Adding new tests and new methods to convert zero By Ana and Terkos devs

--- spec/lcd_kata_spec.rb
+++ spec/lcd_kata_spec.rb
@@ -1,10 +1,28 @@
+require_relative '../lcd_kata'
 
 RSpec.describe 'ConvertNumber' do
-  let(:convert_number) { ConvertNumber.new }
+  let(:convert_number) { ConvertNumber.new('a')}
   describe '#number?' do
     it 'Will let me know if the given character is a number or not' do
-      convert_number.number? = 'a'
       expect(convert_number.number?).to eq(false)
     end
   end
 end
+
+RSpec.describe 'NumberZero' do
+  let(:number_zero) {ConvertNumber.new(0)}
+  describe '#zero' do
+    it 'Will be zero' do
+      array_zero = [[' ', '_', ' '], ['|', ' ', '|'], ['|', '_', '|']]
+      expect (display_lcd).to eq (array_zero)
+    end
+  end
+end
+
+#   _  _     _  _  _  _  _  
+# | _| _||_||_ |_   || ||_|  
+# ||_  _|  | _||_|  ||_| _| 
+
+#  _ 
+# | |
+# |_|     =   0 
